d625997f3f6f6d0f0a61f812a5eaa2ee4cbf68e2
Luis León <luis.leon@pm.me>
2020-06-22 01:56:44 -0400

    Add rustfmt.toml and continue chapter 4

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -3,9 +3,25 @@ fn main() {
 
     change(&mut s);
 
-    print!("{}", s);
+    println!("{}", s);
+
+    let name_s = String::from("John Doe");
+
+    print!("{}", first_word(&name_s));
 }
 
 fn change(string: &mut String) {
     string.push_str(", world");
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
 }
