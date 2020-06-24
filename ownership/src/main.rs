8df9e26ba3c65b71099115b3f0d1457dc3b1642d
Luis León <luis.leon@pm.me>
2020-06-24 00:10:10 -0400

    Update ownership chapter

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -5,23 +5,31 @@ fn main() {
 
     println!("{}", s);
 
-    let name_s = String::from("John Doe");
+    let name = String::from("John Doe");
 
-    print!("{}", first_word(&name_s));
+    print!("{}", first_word(&name));
+
+    print!("{}", first_word("Johnny"));
+
+    let a = [1, 2, 3, 4, 5];
+
+    let slice = &a[1..3];
+
+    print!("{:?}", slice);
 }
 
 fn change(string: &mut String) {
     string.push_str(", world");
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[..i];
         }
     }
 
-    s.len()
+    &s[..]
 }
